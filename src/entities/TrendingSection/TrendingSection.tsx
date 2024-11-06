5f9ee0122a794a5381f9a6be668343abe49d4027
vladochka2812 <vladochkamoroz2812@gmail.com>
2024-11-06 17:29:53 +0200

    remove padding

--- src/entities/TrendingSection/TrendingSection.tsx
+++ src/entities/TrendingSection/TrendingSection.tsx
@@ -10,7 +10,6 @@ import { FilmCardVariant } from '../../shared/FilmCard/model/model';
 
 export const TrendingSection = () => {
   const [selectedTab, setSelectedTab] = useState<string>(TrendingDate[0]);
-  
 
   const path = useMemo(() => {
     return selectedTab === TrendingDate[0] ? PathType.day : PathType.week;
@@ -27,7 +26,7 @@ export const TrendingSection = () => {
   }, [path, dispatch]);
 
   return (
-    <div className="flex flex-col lg:max-w-[1400px] max-w-[100vw] py-[30px] px-10">
+    <div className="flex flex-col lg:max-w-[1400px] max-w-[100vw] py-[30px]">
       <div className="flex justify-start items-center px-5">
         <h2 className="text-[24px] font-semibold mr-5">Trending</h2>
         <TabMenu
